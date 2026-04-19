d73c1767c973fb446c61a2656f36a710b7b5173d
TommyItzhar <itzhar.tommy@gmail.com>
2026-04-19 23:42:19 +0300

    chore: upgraded code architecture 19.04.26

---
+++ frontend/eslint.config.js
@@ -0,0 +1,21 @@
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+
+export default tseslint.config(
+  { ignores: ['dist/**'] },
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+);
